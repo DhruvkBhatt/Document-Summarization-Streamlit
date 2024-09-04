b2761a9cd76805cc3f75eef366c7b2708fbe4223
Dhruv <bdhruv32@gmail.com>
2024-09-04 13:39:48 +0530

    Added more feature like length

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from transformers import pipeline
 import torch
 import base64
 import os
+from streamlit_pdf_viewer import pdf_viewer
 
 #model and tokenizer loading
 checkpoint = "LaMini-Flan-T5-248M"
@@ -39,13 +40,20 @@ def file_preprocessing(file):
     return final_texts
 
 #LLM pipeline
-def llm_pipeline(filepath):
+def llm_pipeline(filepath,min_length,max_length):
+    # max_length assign from streamlit app
+    # Summarization pipeline using T5 model and in Summarization print the summary in asynchrounous way
+    # max_length = st.sidebar.slider("Max Summary Length", min_value=50, max_value=500, value=200)
+    # min_length = st.sidebar.slider("Min Summary Length", min_value=50, max_value=max_length-1, value=50)
+    # st.write("Max Length: ", max_length, "Min Length: ", min_length)
     pipe_sum = pipeline(
         'summarization',
-        model = base_model,
-        tokenizer = tokenizer,
-        max_length = 500, 
-        min_length = 50)
+        model=base_model,
+        tokenizer=tokenizer,
+        max_length=max_length,
+        min_length=min_length,
+        use_fast=True
+    )
     input_text = file_preprocessing(filepath)
     result = pipe_sum(input_text)
     result = result[0]['summary_text']
@@ -68,22 +76,29 @@ def displayPDF(file):
 st.set_page_config(layout="wide")
 
 def main():
-    st.title("Document Summarization App using Langauge Model")
+    st.title("Document Summarization App")
+    
+    max_length = st.sidebar.slider("Max Summary Length", min_value=50, max_value=1000, value=200)
+    min_length = st.sidebar.slider("Min Summary Length", min_value=50, max_value=max_length-1, value=50)
 
     uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
 
     if uploaded_file is not None:
+        
         if st.button("Summarize"):
             col1, col2 = st.columns(2)
             filepath = "data/"+uploaded_file.name
             with open(filepath, "wb") as temp_file:
                 temp_file.write(uploaded_file.read())
             with col1:
-                st.info("Uploaded File")
-                pdf_view = displayPDF(filepath)
+                pdf_viewer(input=filepath, height=600, width=1000)
+            # pdf_viewer(input=filepath, height=600, width=1000)
+            # with col1:
+            #     st.info("Uploaded File")
+            #     pdf_view = displayPDF(filepath)
 
             with col2:
-                summary = llm_pipeline(filepath)
+                summary = llm_pipeline(filepath,min_length,max_length)
                 st.info("Summarization Complete")
                 st.success(summary)
 
